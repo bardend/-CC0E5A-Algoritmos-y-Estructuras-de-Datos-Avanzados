1ca19183a6f6f7392afbde24d84a8c080cf82276
bardend <ricardo.ulloa.v@uni.pe>
2025-06-15 08:33:55 -0500

    Instane of class entry for end-point, Line 5 : Template<Params> (typename features = double, typename identifier = std::string)

--- Project3/m_tree.cpp
+++ Project3/m_tree.cpp
@@ -2,13 +2,14 @@
 #include <iostream>
 #include "entry.hpp"
 
-    
 using params = MTreeParams<double, std::string, 5>;
 
-using LeafEntry = leaf_entry<params>;
+using leaf_entry_t = leaf_entry<params>;
+using internal_entry_t = internal_entry<params>;
+
 using entry_leaf_type = std::unique_ptr<leaf_entry<params>>;
 using entry_inter_type = std::unique_ptr<internal_entry<params>>;
-
+using entry_type = std::unique_ptr<entry<params>>;
 
 using space = metric_space<params>;
 auto manhattan = space::manhattan;
@@ -16,26 +17,40 @@ auto euclidean = space::euclidean;
 space manhattan_space(manhattan);
 space euclidean_space(euclidean);
 
+template <typename... args_t>
+std::unique_ptr<leaf_entry_t> make_leaf(args_t&&... args) {
+    return std::make_unique<leaf_entry_t>(std::forward<args_t>(args)...);
+}
+
+// crea un internal_entry
+template <typename... args_t>
+std::unique_ptr<internal_entry_t> make_inter(args_t&&... args) {
+    return std::make_unique<internal_entry_t>(std::forward<args_t>(args)...);
+}
+
+// crea un entry
+template <typename... args_t>
+std::unique_ptr<entry<params>> make_entry(args_t&&... args) {
+    return std::make_unique<entry<params>>(std::forward<args_t>(args)...);
+}
+
 int main() {
+    ////////////////////////////////////////End_point//////////////////////////
     std::vector<double> features1 = {1.0, 2.0};
     std::vector<double> features2 = {0.0, 0.0};
     std::string xd = "xd";
 
-    auto make_leaf = [](auto&&... args) {
-        return std::make_unique<LeafEntry>(std::forward<decltype(args)>(args)...);
-    };
 
-    auto e1 = std::make_unique<entry<params>>(features1, xd);
+    entry_type e1 = make_entry(features1, xd);
+    entry_type e2 = make_entry(features2, xd); 
+    ////////////////////////////////////////////////////////////////////////////
 
-    auto ee = make_leaf(*e1);
-    std::unique_ptr<entry<params>> e2 = std::make_unique<entry<params>>(features2, xd);
-    std::cout << e1->distance_to(*e2, manhattan_space) << std:: endl;
 
     entry_leaf_type entry_leaf = make_leaf(*e2);
-    entry_inter_type entry_inter = std::make_unique<internal_entry<params>>(*e2, nullptr, 1.5);
+    entry_inter_type entry_inter = make_inter(*e2, nullptr, 1.5);
 
+    std::cout << e1->distance_to(*e2, manhattan_space) << std::endl;
     std::cout << entry_inter->distance_to(*e1, manhattan_space) << std::endl;
     std::cout << entry_leaf->distance_to(*e1, manhattan_space) << std::endl;
-
     return 0;
 }
